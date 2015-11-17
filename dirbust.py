0f47492f8af49947452d09a17c92886b46f3e5a3
N0-Operation <leviathan2u@gmail.com>
2015-11-17 13:59:28 +0100

    fixing some bugs

--- dirbust.py
+++ dirbust.py
@@ -16,18 +16,18 @@ found = []
 print "INFO: Starting dirb scan for " + url
 for folder in folders:
     for filename in os.listdir(folder):
-
-	outfile = " -o " + "/root/scripts/recon_enum/results/exam/" + name + "_dirb_" + filename
-	DIRBSCAN = "dirb %s %s/%s %s -S -r" % (url, folder, filename, outfile)
+        outfile = " -o " + "./results/" + name + "_dirb_" + filename
+        DIRBSCAN = "dirb %s %s/%s %s -S -r" % (url, folder, filename, outfile)
+        print DIRBSCAN
         try:
-	    results = subprocess.check_output(DIRBSCAN, shell=True)
-	    resultarr = results.split("\n")
-	    for line in resultarr:
-	        if "+" in line:
-		    if line not in found:
-		        found.append(line)
-	except:
-	    pass
+            results = subprocess.check_output(DIRBSCAN, shell=True)
+            resultarr = results.split("\n")
+            for line in resultarr:
+                if "+" in line:
+                    if line not in found:
+                        found.append(line)
+        except:
+            pass
 
 try:
     if found[0] != "":
@@ -35,4 +35,4 @@ try:
         for item in found:
             print "   " + item
 except:
-    print "INFO: No items found during dirb scan of " + url		
+    print "INFO: No items found during dirb scan of " + url
