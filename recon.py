90bead768f7f111c36deaaa57d1c0321fb54ae21
0x90 <me@myown.com>
2015-11-24 13:08:57 +0100

    created separate functions module and new main for naming convention.

--- recon.py
+++ recon.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-
-
-'''
- Author: Gerben Visser (0x90) -- @N0_Operation
- A forked project of reconscan.py -- a recon/enumeration script by Mike Czumak (T_v3rn1x) -- @SecuritySift
-'''
-
-
 import subprocess
 import multiprocessing
 import os
@@ -164,24 +155,3 @@ def nmapScan(ip_address):
 
     print "INFO: TCP/UDP Nmap scans completed for " + ip_address
     return
-
-
-# grab the discover scan results and start scanning up hosts
-print "////////////////////////////////////////////////////////////"
-print "///                 Enumeration script                   ///"
-print "///                         --                           ///"
-print "///                                                      ///"
-print "///                 0x90:N0_Operation                    ///"
-print "////////////////////////////////////////////////////////////"
-
-if __name__ == '__main__':
-    try:
-        f = open('./ips', 'r')  # CHANGE THIS!! grab the alive hosts from the discovery scan for enum
-        for scanip in f:
-            jobs = []
-            p = multiprocessing.Process(target=nmapScan, args=(scanip,))
-            jobs.append(p)
-            p.start()
-        f.close()
-    except:
-        print "Is there a file 'ips' containing the ipadresses you wanna scan?"
