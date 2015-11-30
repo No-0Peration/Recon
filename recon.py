aa5464311a29d8072380559329d6666f33c22dde
N0-Operation <leviathan2u@gmail.com>
2015-11-30 10:39:58 +0100

    normalized NSE script settings in HTTPscan job

--- recon.py
+++ recon.py
@@ -31,7 +31,7 @@ def httpEnum(ip_address, port):
     print "INFO: Detected http on " + ip_address + ":" + port
     print "INFO: Performing nmap web script scan for " + ip_address + ":" + port
     checkpath("./results/nmap")
-    HTTPSCAN = "nmap -sV -Pn -vv -p %s --script-args=unsafe=1 --script=http-iis-webdav-vuln.nse,http-vmware-path-vuln.nse,http-vuln-cve2006-3392.nse,http-vuln-cve2009-3960.nse,http-vuln-cve2010-0738.nse,http-vuln-cve2010-2861.nse,http-vuln-cve2011-3192.nse,http-vuln-cve2011-3368.nse,http-vuln-cve2012-1823.nse,http-vuln-cve2013-0156.nse,http-vuln-cve2013-7091.nse,http-vuln-cve2014-2126.nse,http-vuln-cve2014-2127.nse,http-vuln-cve2014-2128.nse,http-vuln-cve2014-2129.nse,http-vuln-cve2014-8877.nse,http-vuln-cve2015-1427.nse,http-vuln-cve2015-1635.nse,http-vuln-misfortune-cookie.nse,http-vuln-wnr1000-creds.nse,http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oN ./results/%s_http.nmap.%s %s" % (
+    HTTPSCAN = "nmap -sV -Pn -vv -p %s --script-args=unsafe=1 --script=*http*vuln*.nse,http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oN ./results/%s_http.nmap.%s %s" % (
     port, ip_address, port, ip_address)
     results = subprocess.check_output(HTTPSCAN, shell=True)
     DIRBUST = "./dirbust.py http://%s:%s %s" % (ip_address, port, ip_address)  # execute the python script
