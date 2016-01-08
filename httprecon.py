9407a66981d5dfd4ea6cd7c283d1483e6d8f6226
dtctd <gyxwddq8mbtf4h7k>
2016-01-08 11:01:33 +0100

    .

--- httprecon.py
+++ httprecon.py
@@ -12,7 +12,7 @@ print "INFO: Performing nmap HTTP script scan for {0} : {1}".format(ip_address, 
 HTTPSCAN = "nmap -sV -Pn -vv -p {0} --script-args=unsafe=1 --script=http-enum,http-feed,http-open-proxy,http-headers,http-cors,http-server-header,http-php-version,http-form-brute,http-iis-short-name-brute,http-waf-fingerprint,http-auth,http-trace,http-iis-webdav-vuln,http-useragent-tester,http-vuln-cve2011-3368,http-userdir-enum,http-passwd,http-csrf,http-wordpress-enum,http-frontpage-login,http-dombased-xss,http-phpself-xss,http-sql-injection,http-drupal-enum-users,http-referer-checker,http-vuln-cve2009-3960,http-methods,http-open-redirect,http-vuln-cve2011-3192,http-stored-xss,http-vuln-cve2013-0156,http-put,http-proxy-brute,http-rfi-spider,http-method-tamper,http-phpmyadmin-dir-traversal -oN ./results/{1}/{1}_http.nmap.{0} {1}".format(port, ip_address)
 results = subprocess.check_output(HTTPSCAN, shell=True)
 outfile = "results/{0}/{0}_HTTPrecon.txt".format(ip_address)
-DIRBUST = "./dirbust.py http://{0}:{1} {0}".format(ip_address, port)  # execute the python script
+DIRBUST = "./dirbust.py http://{0}:{1} {0}_dirbust".format(ip_address, port)  # execute the python script
 subprocess.call(DIRBUST, shell=True)
 NIKTOSCAN = "nikto -host {0} -p {1} > ./results/{0}/{0}.{1}_nikto".format(ip_address, port)
 subprocess.call(NIKTOSCAN, shell=True)
