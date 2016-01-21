120ab169f418c56767095433166d6ed217cd6e69
0x90 <me@myown.com>
2016-01-21 09:26:12 +0100

    Fixing the master branch

--- reconscan.py
+++ reconscan.py
@@ -58,7 +58,7 @@ def scanner(ip_address):
 
     if not recon.checknmaprun(ip_address, "_nmap_scan_import.xml"):
         print("INFO: {0} not scanned or interrupted, starting new TCP nmap scan".format(ip_address))
-        tcpscan = "nmap -vv -Pn -A -O -sS -sV -T4 --top-ports 100 --open -oN './results/{0}/{0}.nmap' -oX './results/{0}/{0}_nmap_scan_import.xml' {0}".format(ip_address)
+        tcpscan = "nmap -vv -Pn -A -O -sS -sV -T4 -p- --open -oN './results/{0}/{0}.nmap' -oX './results/{0}/{0}_nmap_scan_import.xml' {0}".format(ip_address)
         with open(os.devnull, "w") as f:
             subprocess.call(tcpscan, shell=True, stdout=f)
         tcpresults = file("./results/{0}/{0}_nmap_scan_import.xml".format(ip_address), "r")
@@ -138,6 +138,7 @@ print "///                          by                          ///"
 print "///          0x90:N0_Operation & B0x41S & DTCTD          ///"
 print "////////////////////////////////////////////////////////////"
 
+print "Heel veel succes met je Examen Bas!! doorzetten he!"
 if __name__ == '__main__':
     try:
         recon.checkpath("./results/")
