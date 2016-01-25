16a7e94442df852059fbf66b39c3a53baf93588e
0x90 <me@myown.com>
2016-01-25 11:44:45 +0100

    split tcp from udp scan removed some bugs.

--- reconscan.py
+++ reconscan.py
@@ -11,29 +11,10 @@ import gzip
 import re
 import recon  # All functions called by the main scanner function
 
-# print '\033[1;30mGray like Ghost\033[1;m'
-# print '\033[1;31mRed like Radish\033[1;m'
-# print '\033[1;32mGreen like Grass\033[1;m'
-# print '\033[1;33mYellow like Yolk\033[1;m'
-# print '\033[1;34mBlue like Blood\033[1;m'
-# print '\033[1;35mMagenta like Mimosa\033[1;m'
-# print '\033[1;36mCyan like Caribbean\033[1;m'
-# print '\033[1;37mWhite like Whipped Cream\033[1;m'
-# print '\033[1;38mCrimson like Chianti\033[1;m'
-# print '\033[1;41mHighlighted Red like Radish\033[1;m'
-# print '\033[1;42mHighlighted Green like Grass\033[1;m'
-# print '\033[1;43mHighlighted Brown like Bear\033[1;m'
-# print '\033[1;44mHighlighted Blue like Blood\033[1;m'
-# print '\033[1;45mHighlighted Magenta like Mimosa\033[1;m'
-# print '\033[1;46mHighlighted Cyan like Caribbean\033[1;m'
-# print '\033[1;47mHighlighted Gray like Ghost\033[1;m'
-# print '\033[1;48mHighlighted Crimson like Chianti\033[1;m'
-
 
 def scanner(ip_address):
     # Start function which takes ip_address to scan as argument
     ip_address = str(ip_address)
-    serv_dict = {}
     recon.checkpath("./results/{0}".format(ip_address))
 
     if not recon.checknmaprun(ip_address, "_nmap_scan_import.xml"):
@@ -48,7 +29,8 @@ def scanner(ip_address):
         tcpresults = file("./results/{0}/{0}_nmap_scan_import.xml".format(ip_address), "r")
         lines = tcpresults
 
-    # The forloop below parses the nmap results and looks for open service on which it knows to act.
+    # The forloop below parses the TCP nmap results and looks for open service on which it knows to act.
+    serv_dict = {}
     for line in lines:
         ports = []
         if ("tcp" in line) and ("open" in line) and ("service name=" in line) and not ("Discovered" in line):
@@ -62,6 +44,9 @@ def scanner(ip_address):
 
             ports.append(port)
             serv_dict[service] = ports  # add service to the dictionary along with the associated port(2)
+
+        for port in ports:
+            print('\033[1;32m[*]  Open TCP port {0} found on {1}\033[1;m'.format(port, ip_address))
 
     # Go through the service dictionary to call additional targeted enumeration functions
     for serv in serv_dict:
@@ -106,8 +91,7 @@ def scanner(ip_address):
             for port in ports:
                 port = port.split("/")[0]
                 recon.multProc(recon.telnetEnum, ip_address, port)
-        for port in ports:
-            print('\033[1;32m[*]  Open port {0} found on {1}\033[1;m'.format(port, ip_address))
+
 
     if not recon.checknmaprun(ip_address, "U_nmap_scan_import.xml"):
         print('\033[1;34m[*]  Starting new UDP nmap scan for {0}\033[1;m'.format(ip_address))
@@ -120,6 +104,70 @@ def scanner(ip_address):
         print('\033[1;34m[*]  {0} already scanned for UDP ports...\033[1;m'.format(ip_address))
         udpresults = file("./results/{0}/{0}U_nmap_scan_import.xml".format(ip_address), "r")
         lines = udpresults
+
+     # The forloop below parses the UDP nmap results and looks for open service on which it knows to act.
+    serv_dict = {}
+    for line in lines:
+        ports = []
+        if ("udp" in line) and ("open" in line) and ("service name=" in line) and not ("Discovered" in line):
+            port = (re.search("portid=\"(.*?)\"", line))
+            service = (re.search("service name=\"(.*?)\"", line))
+            port = (port.group().split("\""))[1]
+            service = (service.group().split("\""))[1]
+
+            if service in serv_dict:
+                ports = serv_dict[service]  # if the service is already in the dict, grab the port list
+
+            ports.append(port)
+            serv_dict[service] = ports  # add service to the dictionary along with the associated port(2)
+
+        for port in ports:
+            print('\033[1;32m[*]  Open UDP port {0} found on {1}\033[1;m'.format(port, ip_address))
+
+    # Go through the service dictionary to call additional targeted enumeration functions
+    for serv in serv_dict:
+        ports = serv_dict[serv]
+        if serv == "http" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.httpEnum, ip_address, port)
+        elif serv == "ssl/http" or "https" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.httpEnum, ip_address, port)
+        elif "ssh" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.sshEnum, ip_address, port)
+        elif "smtp" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.smtpEnum, ip_address, port)
+        elif "snmp" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.snmpEnum, ip_address, port)
+        elif "domain" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.dnsEnum, ip_address, port)
+        elif "ftp" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.ftpEnum, ip_address, port)
+        elif "microsoft-ds" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.smbEnum, ip_address, port)
+        elif "ms-sql" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.mssqlEnum, ip_address, port)
+        elif "telnet" in serv:
+            for port in ports:
+                port = port.split("/")[0]
+                recon.multProc(recon.telnetEnum, ip_address, port)
+
 
     print('\033[1;34m[*]  TCP/UDP Nmap scans completed for {0}\033[1;m'.format(ip_address))
     return
