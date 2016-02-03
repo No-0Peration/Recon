8a278e5270e88231761dbe0750d3d19e6c95161c
0x90 <me@myown.com>
2016-02-03 22:53:16 +0100

    Bug resolving

--- telnetrecon.py
+++ telnetrecon.py
@@ -10,7 +10,7 @@ ip_address = sys.argv[1].strip()
 port = sys.argv[2].strip()
 
 print('\033[1;34m[*]  Performing hydra TELNET scan against {0}:{1}\033[1;m'.format(ip_address, port))
-HYDRA = "hydra -L /usr/share/wordlists/metasploit/unix_users.txt -P /usr/share/wordlists/rockyou.txt -f -o /root/scripts/recon_enum/results/%s/%s_telnethydra.txt -u %s -s %s telnet" % (ip_address, ip_address, ip_address, port)
+HYDRA = "hydra -L /usr/share/wordlists/metasploit/unix_users.txt -P /usr/share/wordlists/rockyou.txt -f -o ./results/%s/%s_telnethydra.txt -u %s -s %s telnet" % (ip_address, ip_address, ip_address, port)
 try:
     results = subprocess.check_output(HYDRA, shell=True)
     resultarr = results.split("\n")
