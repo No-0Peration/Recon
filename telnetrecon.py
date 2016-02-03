509d6e48951d3aca34abbfcb0ad097036127cf89
0x90 <me@myown.com>
2016-02-03 21:40:53 +0100

    Bug resolving

--- telnetrecon.py
+++ telnetrecon.py
@@ -10,7 +10,7 @@ ip_address = sys.argv[1].strip()
 port = sys.argv[2].strip()
 
 print('\033[1;34m[*]  Performing hydra TELNET scan against {0}:{1}\033[1;m'.format(ip_address, port))
-HYDRA = "hydra -L /usr/share/wordlists/metasploit/unix_users.txt -P /usr/share/wordlists/rockyou.txt -f -o /root/scripts/recon_enum/results/%s_telnethydra.txt -u %s -s %s telnet" % (ip_address, ip_address, port)
+HYDRA = "hydra -L /usr/share/wordlists/metasploit/unix_users.txt -P /usr/share/wordlists/rockyou.txt -f -o /root/scripts/recon_enum/results/%s/%s_telnethydra.txt -u %s -s %s telnet" % (ip_address, ip_address, ip_address, port)
 try:
     results = subprocess.check_output(HYDRA, shell=True)
     resultarr = results.split("\n")
@@ -21,9 +21,9 @@ except:
     print('\033[1;34m[*]  No valid TELNET credentials found\033[1;m')
 
 print('\033[1;34m[*]  Performing nmap TELNET script scan for {0}:{1}\033[1;m'.format(ip_address, port))
-TELNETSCAN = "nmap -sV -Pn -vv -p %s --script=telnet-* -oN './results/%s_telnet.nmap' %s" % (port, ip_address, ip_address)
+TELNETSCAN = "nmap -sV -Pn -vv -p %s --script=telnet-* -oN './results/%s/%s_telnet.nmap' %s" % (port, ip_address, ip_address, ip_address)
 results = subprocess.check_output(TELNETSCAN, shell=True)
-outfile = "results/" + ip_address + "_telnetrecon.txt"
+outfile = "results/{0}/{0}_telnetrecon.txt".format(ip_address)
 f = open(outfile, "w")
 f.write(results)
 f.close
