03bd2438ec424a61a3d76670e84199a307602a1b
0x90 <me@myown.com>
2016-07-19 10:29:58 +0200

    cleanup

--- Modules/httprecon.py
+++ Modules/httprecon.py
@@ -46,7 +46,7 @@ try:
     resultsnikto = subprocess.check_output(NIKTOSCAN, shell=True)
     recon.logparsernikto(resultsnikto)
 
-    SSLSCAN = "sslscan {0}:{1}".format(ip_address, port)
+    SSLSCAN = "sslscan --no-colour {0}:{1}".format(ip_address, port)
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
     print('\033[1;37m[-]  |     Starting SSL scan for {0}:{1} \033[1;m'.format(ip_address, port))
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
